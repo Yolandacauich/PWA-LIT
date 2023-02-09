79f42b58e22a7fa11186f59f203772fd8f486224
Yolandacauich <yolanda.19070017@itsmotul.edu.mx>
2023-02-08 21:31:11 -0600

    index

--- Intro to Lit_1/my-element.ts
+++ Intro to Lit_1/my-element.ts
@@ -1,15 +1,11 @@
 import {LitElement, html} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import {customElement} from 'lit/decorators.js';
 
 @customElement('my-element')
 export class MyElement extends LitElement {
-  @property()
-  version = 'COMPLETED';
-
   render() {
     return html`
-    <p>Welcome to the Lit tutorial!</p>
-    <p>This is the ${this.version} code.</p>
+      <p>Hello world! From my-element.</p>
     `;
   }
 }
