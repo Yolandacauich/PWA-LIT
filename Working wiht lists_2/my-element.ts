2dcffc49114f8f8ed95c8c1aadd7f0d442662dfe
Yolandacauich <yolanda.19070017@itsmotul.edu.mx>
2023-02-08 22:25:18 -0600

    index2

--- Working wiht lists_2/my-element.ts
+++ Working wiht lists_2/my-element.ts
@@ -1,27 +1,20 @@
 import {LitElement, html} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import {customElement, state} from 'lit/decorators.js';
+import {map} from 'lit/directives/map.js';
 
 @customElement('my-element')
 class MyElement extends LitElement {
+  @state()
+  items = new Set(['manzana', 'Plátano', 'Uva', 'Naranja', 'Lima'])
+
   render() {
     return html`
-      <h1>Rendering lists with Lit</h1>
-      <p>Lit has built-in support for any iterables!</p>
-      <h2>Array</h2>
-      <p>
-        ${['✨', '🔥', '❤️', '😻']}
-      </p>
-      <h2>Set</h2>
-      <p>
-        ${new Set(['A', 'B', 'C'])}
-      </p>
-      <h2>Generator</h2>
-      <p>
-        ${(function* () {
-            for (let i = 1; i < 4; i++) yield i;
-        })()}
-      </p>
+      <p>MISFURTOS</p>
+      <ul>
+        ${map(this.items, (item) => html`<li>${item}</li>`)}
+      </ul>
     `;
   }
 }
+
 
