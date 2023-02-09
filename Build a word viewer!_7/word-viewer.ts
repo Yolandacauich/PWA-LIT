096ac4d757e6c142d40bf3f5c08ec50ab3d3d1d3
Yolandacauich <yolanda.19070017@itsmotul.edu.mx>
2023-02-09 00:04:09 -0600

    index7

--- Build a word viewer!_7/word-viewer.ts
+++ Build a word viewer!_7/word-viewer.ts
@@ -1,15 +1,11 @@
 import { html, LitElement } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 
 @customElement('word-viewer')
 class WordViewer extends LitElement {
-  render() {
-    return html`<pre>A super expressive and efficient template!</pre>`
-  }
-}
+  @property() words = 'initial value';
 
-declare global {
-  interface HTMLElementTagNameMap {
-    "word-viewer": WordViewer
+  render() {
+    return html`<pre>${this.words}</pre>`
   }
 }
