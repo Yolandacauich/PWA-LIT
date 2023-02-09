377c45cdce0f1bcb060c557c5b6e272719b5b71d
Yolandacauich <yolanda.19070017@itsmotul.edu.mx>
2023-02-08 23:17:19 -0600

    index4

--- Custom attribute converters_4/date-dislay.ts
+++ Custom attribute converters_4/date-dislay.ts
@@ -1,18 +1,20 @@
-import {LitElement, html} from 'lit';
+import {LitElement, html, PropertyValues} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
-import {dateConverter} from './date-converter.js';
 
 @customElement('date-display')
 export class DateDisplay extends LitElement {
-  @property({converter: dateConverter})
+  @property({attribute: false})
   date = new Date();
+
+  @property({type: String, attribute: 'date-str'})
+  dateStr = '';
 
   render() {
     const locale = 'en-US';
     const options: Intl.DateTimeFormatOptions =
         {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
     return html`
-      <p>La fecha es: ${this.date.toLocaleDateString(locale, options)}</p>
+      <p>The given date is: ${this.date.toLocaleDateString(locale, options)}</p>
     `;
   }
 }
